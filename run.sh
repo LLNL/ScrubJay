b29af39c20554f9ad7839afe1c4e27c965784da2
Alfredo Gimenez <gimenez1@llnl.gov>
2016-07-29 17:26:41 -0700

    Cleaned up Any => Units conversion
    Former-commit-id: 78db67e2000c47d82a21d6b0dec08cca701c1f93

--- run.sh
+++
@@ -1,1 +0,0 @@
-spark-submit --class $1 ./target/scala-2.10/Scrubjay-assembly-1.0.jar
--- src/main/scala/scrubjay/units/Units.scala
+++ src/main/scala/scrubjay/units/Units.scala
@@ -5,6 +5,7 @@ import scrubjay.datasource._
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
+import scala.language.implicitConversions
 import scala.reflect._
 import scala.reflect.ClassTag
 
@@ -14,28 +15,30 @@ abstract class Units[T <: Units[T] : ClassTag] extends Serializable {
 
 case class Identifier(v: Any) extends Units[Identifier]
 case class Seconds(v: Double) extends Units[Seconds]
-case class Timestamp(v: Double) extends Units[Timestamp]
 
-case class UnitList[T](l: List[T]) extends Units[UnitList[T]]
+case class UnitList[T](v: List[T]) extends Units[UnitList[T]]
 
 object Units {
-  // TODO: Make this less ugly
+
+  // Implicit converters for Any to types accepted by units (e.g. Double)
+  implicit def any2Double(a: Any): Double = a match {
+    case n: Number => n.doubleValue
+    case s: String => s.toDouble
+    case _ => throw new RuntimeException(s"Cannot cast $a to Double!")
+  }
+
   def keyRaw2KeyUnits(ik: String, iv: Any, it: ClassTag[_], mm: MetaMap): (String, Units[_]) = (ik, iv, it) match {
+    // Canonical units
     case (k, v, t) if t == classTag[Identifier] => (k, Identifier(v))
-    case (k, v, t) if t == classTag[Seconds] => v match {
-      case n: Number => (k, Seconds(n.doubleValue))
-      case _ => throw new UnsupportedOperationException(s"Bad input $k for class $t")
-    }
-    case (k, v, t) if t == classTag[Timestamp] => v match {
-      case n: Number => (k, Timestamp(n.doubleValue))
-      case _ => throw new UnsupportedOperationException(s"Bad input $k for class $t")
-    }
+    case (k, v, t) if t == classTag[Seconds] => (k, Seconds(v))
+
+    // Collection units
     case (k, v, t) if t == classTag[UnitList[_]] => v match {
-      case l: List[_] =>
-        val childTag = mm(k).units.children.head.tag
-        (k, UnitList(l.map(iv1 => keyRaw2KeyUnits(k, iv1, childTag, mm)._2)))
+      case l: List[_] => (k, UnitList(l.map(iv1 => keyRaw2KeyUnits(k, iv1, mm(k).units.children.head.tag, mm)._2)))
     }
-    case (k, v, t) => throw new UnsupportedOperationException("Can't find classTag for " + t)
+
+    // Error
+    case (k, v, t) => throw new RuntimeException("Can't find classTag for " + t)
   }
 
   def rawRDDToUnitsRDD(sc: SparkContext, rawRDD: RDD[RawDataRow], metaMap: MetaMap): RDD[DataRow] = {
