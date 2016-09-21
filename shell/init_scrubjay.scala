42f3b0fab080f4c688f17254a2adad085d4e4357
Alfredo Gimenez <gimenez1@llnl.gov>
2016-09-21 12:12:56 -0700

    Look for CQLSH_HOST, default to localhost
    Former-commit-id: 2dbe721de29e261fdac52dc4896ef08a0cc54d16

--- shell/init_scrubjay.scala
+++ shell/init_scrubjay.scala
@@ -5,7 +5,9 @@ import scrubjay.datasource._
 
 import com.datastax.spark.connector._
 
+val cassandraHost = sys.env.get("CQLSH_HOST").getOrElse("localhost")
+
 val sjs = new ScrubJaySession(
-  cassandra_connection = Some(CassandraConnection(hostname="sonar11")))
+  cassandra_connection = Some(CassandraConnection(hostname = cassandraHost))
 
 println("ScrubJaySession available as sjs")
