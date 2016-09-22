394c2bd350531fb0f427b78107c5286e2b17dbe5
Alfredo Gimenez <gimenez1@llnl.gov>
2016-09-22 11:36:13 -0700

    Fixed typo in init script and added imports for implicit functions
    Former-commit-id: 8cf774316f5f53fbed8f75bf909dc4511248b0fd

--- shell/init_scrubjay.scala
+++ shell/init_scrubjay.scala
@@ -1,13 +1,19 @@
 sc.stop
 
 import scrubjay._
+import scrubjay.meta._
 import scrubjay.datasource._
+
+import scrubjay.datasource.LocalDataSource._
+import scrubjay.datasource.CSVDataSource._
+import scrubjay.datasource.CassandraDataSource._
+
 
 import com.datastax.spark.connector._
 
 val cassandraHost = sys.env.get("CQLSH_HOST").getOrElse("localhost")
 
 val sjs = new ScrubJaySession(
-  cassandra_connection = Some(CassandraConnection(hostname = cassandraHost))
+  cassandra_connection = Some(CassandraConnection(hostname = cassandraHost)))
 
 println("ScrubJaySession available as sjs")
