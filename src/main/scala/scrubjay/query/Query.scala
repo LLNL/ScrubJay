e6abdd5fd3949f6a2959965f2a1b3b8ccc092001
Alfredo Gimenez <gimenez1@llnl.gov>
2016-10-10 13:00:27 -0700

    Transitive joins working, ALL join paths are now discovered, not just the first
    Former-commit-id: b2cba8ad44c0d5bab296224592f0b6df01a1d942

--- src/main/scala/scrubjay/query/Query.scala
+++ src/main/scala/scrubjay/query/Query.scala
@@ -17,94 +17,89 @@ class Query(val sjs: ScrubJaySession,
 
     // Easy case: queried meta entries all contained in a single data source
     lazy val dsWithMetaEntries: Constraint[DataSource] = memoize(args => {
-      val me: Set[MetaEntry] = args.getAs[Set[MetaEntry]](0)
-      val ds: DataSource = args.getAs[DataSource](1)
+      val me = args(0).as[Set[MetaEntry]]
+      val ds = args(1).as[DataSource]
 
       ds match {
-        case dsSat if dsSat.containsMeta(me) => Some(dsSat)
-        case _ => None
+        case dsSat if dsSat.containsMeta(me) => Seq(dsSat)
+        case _ => Seq.empty
       }
     })
 
     // Can we join ds1 and ds2?
     lazy val joinedPair: Constraint[DataSource] = memoize(args => {
-      val ds1 = args.getAs[DataSource](0)
-      val ds2 = args.getAs[DataSource](1)
+      val ds1 = args(0).as[DataSource]
+      val ds2 = args(1).as[DataSource]
 
-      val joined: Option[DataSource] = deriveNaturalJoin(ds1, ds2, sjs)
-
-      if (joined.isDefined)
-        joined
-      else
-        None
+      // TODO: return all possible joins (a,b), (b,a), natural, quanti, etc
+      Seq(
+        deriveNaturalJoin(ds1, ds2, sjs)
+      ).flatten
     })
 
     // Can we join a set of datasources dsSet?
     lazy val joinedSet: Constraint[DataSource] = memoize(args => {
-      val dsSet: Set[DataSource] = args.getAs[Set[DataSource]](0)
+      val dsSet: Set[DataSource] = args(0).as[Set[DataSource]]
 
       dsSet.toSeq match {
 
         // One element
-        case Seq(ds1) => Some(ds1)
+        case Seq(ds1) => Seq(ds1)
 
         // Two elements, check joined pair
-        case Seq(ds1, ds2) => joinedPair(Arguments(ds1, ds2))
+        case Seq(ds1, ds2) => joinedPair(Seq(Arg(ds1), Arg(ds2)))
 
         // More than two elements...
         case head +: tail => {
 
-          // Check if head can join with any of tail
-          val joinedOptionOption = tail.map(t => joinedPair(Arguments(head, t))).find(_.isDefined)
 
-          // If so, check if the tail is a joinableSet
-          if (joinedOptionOption.isDefined) {
+          // joinPair( head, joinSet(tail) )
+          val restThenPair = joinedSet(Seq(Arg(tail.toSet))).flatMap(tailSolution => joinedPair(Seq(Arg(head), Arg(tailSolution))))
 
-            val joined = joinedOptionOption.get.get
-            val joinedTail = joinedSet(Arguments(tail.toSet))
+          // Set of all joinable pairs between head and some t in tail
+          val head2TailPairs = new ArgumentSpace(Seq(Arg(head)), tail.map(Arg(_))).allSolutions(joinedPair)
 
-            // If so, return the head joined with the joinedSet of the tail
-            if (joinedTail.isDefined) {
-              joinedPair(Arguments(joined, joinedTail))
-            }
-            else {
-              None
-            }
+          // joinSet( joinPair(head, t) +: rest )
+          val pairThenRest = head2TailPairs.flatMap(pair => {
+            val pairArgs = pair.arguments.map(_.as[DataSource])
+            val rest = dsSet.filterNot(pairArgs.contains)
+            pair.solutions.flatMap(sol =>  {
+              joinedSet(Seq(Arg(rest + sol)))
+            })
+          })
 
-          }
-          else {
-            None
-          }
+          restThenPair ++ pairThenRest
         }
       }
     })
 
     // Can I derive a datasource from the set of datasources that satisfies my query?
     lazy val dsSetSatisfiesQuery: Constraint[DataSource] = memoize(args => {
-      val query = args.getAs[Set[MetaEntry]](0)
-      val dsSet = args.getAs[Set[DataSource]](1)
+      val query = args(0).as[Set[MetaEntry]]
+      val dsSet = args(1).as[Set[DataSource]]
 
       // Fun case: queried meta entries exist in a data source derived from multiple data sources
+      // TODO: return all unique sets of datasources that satisfy the query
       val dsMeta = dsSet.toSeq.map(_.metaSource.metaEntryMap.values.toSet).reduce(_ union _)
       val metaSatisfied = query.intersect(dsMeta).size == query.size
 
       if (metaSatisfied)
-        joinedSet(Arguments(dsSet))
+        joinedSet(Seq(Arg(dsSet)))
       else
-        None
+        Seq.empty
     })
 
     class ChooseNDataSources(me: Seq[MetaEntry], ds: Seq[DataSource]) extends ArgumentSpace {
       override def enumerate: Iterator[Arguments] = {
         (1 until ds.length+1).toIterator.flatMap(
-          ds.combinations(_).map(c => Arguments(metaEntries, c.toSet))
+          ds.combinations(_).map(c => Seq(Arg(metaEntries), Arg(c.toSet)))
         )
       }
     }
 
     val argSpace = new ChooseNDataSources(metaEntries.toSeq, dataSources.toSeq)
 
-    argSpace.allSolutions(dsSetSatisfiesQuery).flatMap(dsSetSatisfiesQuery(_))
+    argSpace.allSolutions(dsSetSatisfiesQuery).flatMap(_.solutions)
   }
 
 }
