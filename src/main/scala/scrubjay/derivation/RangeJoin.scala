65ad824db63c43a2da5db8c12411d414b4b25c10
Alfredo Gimenez <gimenez1@llnl.gov>
2016-10-24 10:18:55 -0700

    Started RangeJoin
    Former-commit-id: 66de07a4f9fff3b91f74e336659afcf744adf208

--- src/main/scala/scrubjay/derivation/RangeJoin.scala
+++ src/main/scala/scrubjay/derivation/RangeJoin.scala
@@ -1,20 +1,19 @@
 package scrubjay.derivation
 
-import scrubjay.units._
-import scrubjay.metasource._
-import scrubjay.datasource._
+import scrubjay.DataSource
+import scrubjay.datasource.DataRow
 import scrubjay.metabase.MetaDescriptor.DimensionType
-
-import scala.language.existentials
+import scrubjay.metasource.MetaSource
+import scrubjay.units._
 import org.apache.spark.rdd.RDD
 import scrubjay.units.UnitsTag.DomainType
 
-class InterpolationJoin(dso1: Option[DataSource], dso2: Option[DataSource], window: Double) extends Joiner(dso1, dso2) {
+class RangeJoin(dso1: Option[DataSource], dso2: Option[DataSource], window: Double) extends Joiner(dso1, dso2) {
 
   val commonDimensions = MetaSource.commonDimensionEntries(ds1.metaSource, ds2.metaSource)
   val commonContinuousDimensions = commonDimensions.filter(d =>
     d._1.dimensionType == DimensionType.CONTINUOUS &&
-      d._2._1.units.unitsTag.domainType == DomainType.POINT &&
+      d._2._1.units.unitsTag.domainType == DomainType.RANGE &&
       d._2._2.units.unitsTag.domainType == DomainType.POINT).toSeq
   val commonDiscreteDimensions = commonDimensions.filter(_._1.dimensionType == DimensionType.DISCRETE).toSeq
 
@@ -29,41 +28,41 @@ class InterpolationJoin(dso1: Option[DataSource], dso2: Option[DataSource], wind
 
       override lazy val rdd: RDD[DataRow] = {
 
-        val continuousDimColumn1 = commonContinuousDimensions.flatMap{case (d, me) => ds1.metaSource.columnForEntry(me._1)}.head
-        val continuousDimColumn2 = commonContinuousDimensions.flatMap{case (d, me) => ds2.metaSource.columnForEntry(me._2)}.head
+        val continuousDimColumn1 = commonContinuousDimensions.flatMap { case (d, me) => ds1.metaSource.columnForEntry(me._1) }.head
+        val continuousDimColumn2 = commonContinuousDimensions.flatMap { case (d, me) => ds2.metaSource.columnForEntry(me._2) }.head
 
-        val discreteDimColumns1 = commonDiscreteDimensions.flatMap{case (d, me) => ds1.metaSource.columnForEntry(me._1)}
-        val discreteDimColumns2 = commonDiscreteDimensions.flatMap{case (d, me) => ds2.metaSource.columnForEntry(me._2)}
+        val discreteDimColumns1 = commonDiscreteDimensions.flatMap { case (d, me) => ds1.metaSource.columnForEntry(me._1) }
+        val discreteDimColumns2 = commonDiscreteDimensions.flatMap { case (d, me) => ds2.metaSource.columnForEntry(me._2) }
 
         // Key by all values in the current window and next
         val flooredRDD1 = ds1.rdd.keyBy(row =>
-          discreteDimColumns1.map(row) :+ scala.math.floor(row(continuousDimColumn1).asInstanceOf[Continuous].asDouble/window).toInt)
+          discreteDimColumns1.map(row) :+ scala.math.floor(row(continuousDimColumn1).asInstanceOf[Continuous].asDouble / window).toInt)
         val flooredRDD2 = ds2.rdd.keyBy(row =>
-          discreteDimColumns2.map(row) :+ scala.math.floor(row(continuousDimColumn2).asInstanceOf[Continuous].asDouble/window).toInt)
+          discreteDimColumns2.map(row) :+ scala.math.floor(row(continuousDimColumn2).asInstanceOf[Continuous].asDouble / window).toInt)
 
         // Key by all values in the current window and previous
         val ceilRDD1 = ds1.rdd.keyBy(row =>
-          discreteDimColumns1.map(row) :+ scala.math.ceil(row(continuousDimColumn1).asInstanceOf[Continuous].asDouble/window).toInt)
+          discreteDimColumns1.map(row) :+ scala.math.ceil(row(continuousDimColumn1).asInstanceOf[Continuous].asDouble / window).toInt)
         val ceilRDD2 = ds2.rdd.keyBy(row =>
-          discreteDimColumns2.map(row) :+ scala.math.ceil(row(continuousDimColumn2).asInstanceOf[Continuous].asDouble/window).toInt)
+          discreteDimColumns2.map(row) :+ scala.math.ceil(row(continuousDimColumn2).asInstanceOf[Continuous].asDouble / window).toInt)
 
         // Group each
         val floorGrouped = flooredRDD1.cogroup(flooredRDD2)
         val ceilGrouped = ceilRDD1.cogroup(ceilRDD2)
 
         // Create 1 to N mapping from ds1 to ds2 for each
-        val floorMapped = floorGrouped.flatMap{case (k, (l1, l2)) => l1.map(row => (row, l2))}
-        val ceilMapped = ceilGrouped.flatMap{case (k, (l1, l2)) => l1.map(row => (row, l2))}
+        val floorMapped = floorGrouped.flatMap { case (k, (l1, l2)) => l1.map(row => (row, l2)) }
+        val ceilMapped = ceilGrouped.flatMap { case (k, (l1, l2)) => l1.map(row => (row, l2)) }
 
         // Co-group both window mappings and combine their results
         val grouped = floorMapped.cogroup(ceilMapped)
-          .map{case (k, (f, c)) => (k, (f.flatten ++ c.flatten).toSeq)}
+          .map { case (k, (f, c)) => (k, (f.flatten ++ c.flatten).toSeq) }
 
         // Lift the heavies here
         val ds2MetaEntries = grouped.sparkContext.broadcast(ds2.metaSource.metaEntryMap)
         def projection(row: DataRow, keyColumn: String, mappedRows: Seq[DataRow], mappedKeyColumn: String): DataRow = {
 
-          val observations = scala.collection.mutable.Map[String,(Seq[Double], Seq[Units[_]])]()
+          val observations = scala.collection.mutable.Map[String, (Seq[Double], Seq[Units[_]])]()
 
           // Add a single observation to the set of observations
           def addObservation(column: String, x: Double, y: Units[_]): Unit = {
@@ -73,7 +72,7 @@ class InterpolationJoin(dso1: Option[DataSource], dso2: Option[DataSource], wind
             }
           }
 
-          for (mapRow <- mappedRows; mapElem <- mapRow.filterNot{case (k,v) => k == mappedKeyColumn}) {
+          for (mapRow <- mappedRows; mapElem <- mapRow.filterNot { case (k, v) => k == mappedKeyColumn }) {
             addObservation(mapElem._1, mapRow(mappedKeyColumn).asInstanceOf[Continuous].asDouble, mapElem._2)
           }
 
@@ -86,7 +85,7 @@ class InterpolationJoin(dso1: Option[DataSource], dso2: Option[DataSource], wind
           row ++ projectedValues
         }
 
-        grouped.map{case (k, l2) => projection(k, continuousDimColumn1, l2, continuousDimColumn2)}
+        grouped.map { case (k, l2) => projection(k, continuousDimColumn1, l2, continuousDimColumn2) }
       }
     }
   }
