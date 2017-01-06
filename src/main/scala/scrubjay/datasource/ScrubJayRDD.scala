1b96619ae0f76f6d9fddfd5bce593f1fd459931c
Alfredo Gimenez <gimenez1@llnl.gov>
2017-01-05 16:54:24 -0800

    Improved objectbase loading, added storing, and notes for next steps
    Former-commit-id: 6c6f436bdb69b3af4984f152286960db649db060

--- src/main/scala/scrubjay/datasource/ScrubJayRDD.scala
+++ src/main/scala/scrubjay/datasource/ScrubJayRDD.scala
@@ -3,10 +3,32 @@ package scrubjay.datasource
 import org.apache.spark.{Partition, TaskContext}
 import scrubjay.metasource.MetaSource
 import org.apache.spark.rdd.RDD
+import scrubjay.objectbase.ObjectBase
 import scrubjay.units.Units
 
 
 class ScrubJayRDD(fromRdd: ParsedRDD, val metaSource: MetaSource) extends RDD[DataRow](fromRdd) {
+
+  // TODO: Derivation Provenance
+
+  // Original?
+
+  // Derived? What derivations? Arguments?
+
+  // Save this to ScrubJay's objectbase
+  def saveToObjectBase: Unit = {
+
+    // If this is an original object, save it as such
+
+    // If it is a derived object, compute the hash
+    //   for its derivation path and store it as a
+    //   derived object keyed by the hash.
+
+    // RESEARCH QUESTION:
+    //   How many sub-derivations to store? All of them?
+
+    ???
+  }
 
   def this(fromRawRdd: RawRDD, metaSource: MetaSource) {
     this(Units.rawRDDToUnitsRDD(fromRawRdd, metaSource.metaEntryMap), metaSource)
@@ -19,6 +41,7 @@ class ScrubJayRDD(fromRdd: ParsedRDD, val metaSource: MetaSource) extends RDD[Da
   override def compute(split: Partition, context: TaskContext): Iterator[DataRow] = {
     fromRdd.compute(split, context)
   }
+
 }
 
 object ScrubJayRDD {
