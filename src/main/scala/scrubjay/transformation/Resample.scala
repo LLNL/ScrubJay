8a71bcfc223a0a94c090e33a9cd25e042022821e
Alfredo Gimenez <gimenez1@llnl.gov>
2017-04-06 11:21:55 -0700

    resample wip
    Former-commit-id: 09aeb039400fe9eafa3ac10be4c7315d11da78ab

---
+++ src/main/scala/scrubjay/transformation/Resample.scala
@@ -0,0 +1,68 @@
+package scrubjay.transformation
+
+import scrubjay.datasource._
+import scrubjay.metasource._
+import scrubjay.units._
+import scrubjay.metabase.MetaEntry
+
+import scala.language.existentials
+
+
+/*
+ *  Example resamplings:
+ *
+ *    GET (flops) PER (job)
+ *    GET (flops, job) PER (time [period=Seconds(1)])
+ *    GET (flops, job) PER (time [period=Seconds(1), start=10:00, end=11:00])
+ *    TODO: GET (flops, job) PER (node, job)
+ */
+
+case class ResampleMethod[T: Units](metaEntry: MetaEntry, start: Option[T], end: Option[T], period: Option[Double])
+
+case class Resample(dsID: DataSourceID, method: ResampleMethod[_])
+  extends DataSourceID {
+
+  override val metaSource: MetaSource = dsID.metaSource
+
+  override def isValid: Boolean = true
+
+  override def realize: ScrubJayRDD = {
+    val ds = dsID.realize
+
+    // Key each row by the provided metaEntry
+    val column = metaSource.columnForEntry(method.metaEntry).get
+
+    var rdd = ds.rdd.keyBy(row => row(column).asInstanceOf[Continuous].asDouble)
+
+    // If we have a start filter
+    if (method.start.isDefined) {
+      rdd = rdd.filter(
+        _._1.asInstanceOf[Continuous].asDouble >=
+          method.start.get.asInstanceOf[Continuous].asDouble)
+    }
+
+    // If we have an end filter
+    if (method.end.isDefined) {
+      rdd = rdd.filter(
+        _._1.asInstanceOf[Continuous].asDouble >=
+          method.end.get.asInstanceOf[Continuous].asDouble)
+    }
+
+    // If we have a period, transform the key into the period
+    if (method.period.isDefined) {
+      val periodDouble = method.period.asInstanceOf[Continuous].asDouble
+      rdd = rdd.map{case (key, row) =>
+        (key / periodDouble, row)
+      }
+    }
+
+    // Reduce all other columns by the bin (aggregateByKey)
+    val reducer = method.metaEntry.units.unitsTag.reduce _
+    rdd = rdd.map{case (key, rows) => {
+      val aggregatedRow = ???
+      null
+    }}
+
+    new ScrubJayRDD(ds.rdd)
+  }
+}
