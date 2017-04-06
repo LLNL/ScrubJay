9df7928a84fd5d6bbebe068241e2208193ac6a8c
Alfredo Gimenez <gimenez1@llnl.gov>
2017-04-06 10:44:45 -0700

    Added resample test case
    Former-commit-id: 64a6cdf928e78ae54db65e9b1bb8bb31bebc4855

---
+++ src/main/scala/scrubjay/transformation/Resample.scala
@@ -0,0 +1,44 @@
+package scrubjay.transformation
+
+import scrubjay.metabase._
+import scrubjay.metasource._
+import scrubjay.datasource._
+
+import org.apache.spark.rdd.RDD
+
+
+
+/*
+ *  Valid resampling queries:
+ *    GET (flops) PER (job)
+ *    GET (job, flops) PER (time = Seconds(1) FROM time = 10:00 TO time = 11:00)
+ */
+
+case class Resample(dsID: DataSourceID, columns: Seq[String])
+  extends DataSourceID(dsID) {
+
+  def isValid: Boolean = ???
+
+  val metaSource: MetaSource = dsID.metaSource
+
+  def realize: ScrubJayRDD = ???
+
+  /*
+  {
+
+    val ds = dsID.realize
+
+    val rdd: RDD[DataRow] = {
+
+      val reducer = metaEntry.units.unitsTag.reduce _
+
+      ds.map(row => {
+        val mergedVal = reducer(columns.map(row))
+        row.filterNot{case (k, _) => columns.contains(k)} ++ Map(newColumn -> mergedVal)
+      })
+    }
+
+    new ScrubJayRDD(rdd)
+  }
+  */
+}
