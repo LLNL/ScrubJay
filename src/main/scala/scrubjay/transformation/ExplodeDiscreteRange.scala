40f9f4e57287d99b0c8632574615d93cb695a852
Alfredo Gimenez <gimenez1@llnl.gov>
2017-04-24 14:40:29 -0700

    Copypasta from explode
    Former-commit-id: 882563e318fce2c73fc369190febeb812496b7e8

--- src/main/scala/scrubjay/transformation/ExplodeDiscreteRange.scala
+++ src/main/scala/scrubjay/transformation/ExplodeDiscreteRange.scala
@@ -1,10 +1,15 @@
 package scrubjay.transformation
 
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.explode
-import org.apache.spark.sql.types.{ArrayType, MapType}
 import scrubjay.dataset._
 
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.analysis.TypeCheckResult
+import org.apache.spark.sql.catalyst.expressions.{Expression, ExpressionDescription, Generator, UnaryExpression}
+import org.apache.spark.sql.catalyst.expressions.codegen.CodegenFallback
+import org.apache.spark.sql.catalyst.util.{ArrayData, MapData}
+import org.apache.spark.sql.types.{ArrayType, MapType, StructType}
 
 case class ExplodeDiscreteRange(dsID: DatasetID, column: String)
   extends DatasetID(Seq(dsID)) {
@@ -16,8 +21,73 @@ case class ExplodeDiscreteRange(dsID: DatasetID, column: String)
   }
 
   override def realize: DataFrame = {
-    // TODO: update metadata, e.g. a List[String] becomes a String
     val DF = dsID.realize
-    DF.withColumn(column, explode(DF(column)))
+    DF.withColumn(column, ExplodeDiscreteRange.dfExpression(DF(column)))
   }
+}
+
+object ExplodeDiscreteRange {
+
+  @ExpressionDescription(
+    usage = "_FUNC_(expr) - Separates the elements of array `expr` into multiple rows, or the elements of map `expr` into multiple rows and columns.",
+    extended = """
+    Examples:
+      > SELECT _FUNC_(array(10, 20));
+       10
+       20
+  """)
+  case class ExpressionClass(child: Expression)
+    extends UnaryExpression with Generator with CodegenFallback with Serializable {
+
+    override def checkInputDataTypes(): TypeCheckResult = {
+      if (child.dataType.isInstanceOf[ArrayType] || child.dataType.isInstanceOf[MapType]) {
+        TypeCheckResult.TypeCheckSuccess
+      } else {
+        TypeCheckResult.TypeCheckFailure(
+          s"input to function explode should be array or map type, not ${child.dataType}")
+      }
+    }
+
+    override def elementSchema: Schema = child.dataType match {
+      case ArrayType(et, containsNull) =>
+        new StructType()
+          .add("col", et, containsNull)
+      case MapType(kt, vt, valueContainsNull) =>
+        new StructType()
+          .add("key", kt, nullable = false)
+          .add("value", vt, valueContainsNull)
+    }
+
+    override def eval(input: InternalRow): TraversableOnce[InternalRow] = {
+      child.dataType match {
+        case ArrayType(et, _) =>
+          val inputArray = child.eval(input).asInstanceOf[ArrayData]
+          if (inputArray == null) {
+            Nil
+          } else {
+            val rows = new Array[InternalRow](inputArray.numElements())
+            inputArray.foreach(et, (i, e) => {
+              rows(i) = InternalRow(e)
+            })
+            rows
+          }
+        case MapType(kt, vt, _) =>
+          val inputMap = child.eval(input).asInstanceOf[MapData]
+          if (inputMap == null) {
+            Nil
+          } else {
+            val rows = new Array[InternalRow](inputMap.numElements())
+            var i = 0
+            inputMap.foreach(kt, vt, (k, v) => {
+              rows(i) = InternalRow(k, v)
+              i += 1
+            })
+            rows
+          }
+      }
+    }
+
+  }
+
+  def dfExpression(column: Column): Column = withExpr { ExpressionClass(column.expr) }
 }
--- src/main/scala/scrubjay/transformation/package.scala
+++ src/main/scala/scrubjay/transformation/package.scala
@@ -1,4 +1,9 @@
 package scrubjay
 
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.catalyst.expressions.Expression
+
 package object transformation {
+
+  def withExpr(expr: Expression): Column = new Column(expr)
 }
