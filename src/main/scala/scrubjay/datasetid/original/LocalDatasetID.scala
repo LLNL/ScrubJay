8de111f14e9ab5bec2e0ecc4245e731b6d55f89b
Alfredo Adolfo Gimenez <gimenez1@llnl.gov>
2018-07-17 14:45:25 -0700

    Define spark schema for local dataset
    Former-commit-id: 8ef7d361021b2967c1c564981252f0f630eed16a

--- src/main/scala/scrubjay/datasetid/original/LocalDatasetID.scala
+++ src/main/scala/scrubjay/datasetid/original/LocalDatasetID.scala
@@ -1,17 +1,22 @@
 package scrubjay.datasetid.original
 
 import org.apache.spark.sql.{DataFrame, Dataset}
-import scrubjay.datasetid.{ScrubJaySchema}
+import scrubjay.datasetid.{ScrubJaySchema, SparkSchema}
 import scrubjay.dataspace.DimensionSpace
 
 case class LocalDatasetID(dataframe: DataFrame,
                           scrubJaySchema: ScrubJaySchema)
+                         (sparkSchema: SparkSchema = dataframe.schema)
   extends OriginalDatasetID(scrubJaySchema) {
 
   override def isValid(dimensionSpace: DimensionSpace): Boolean = true
 
   override def load: DataFrame = {
-    dataframe
+    if (sparkSchema != dataframe.schema) {
+      spark.createDataFrame(dataframe.rdd, sparkSchema)
+    } else {
+      dataframe
+    }
   }
 }
 
