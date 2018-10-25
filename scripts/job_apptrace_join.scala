ae0550011fec14994595aeda18a68a4714aba402
Alfredo Adolfo Gimenez <gimenez1@llnl.gov>
2018-10-25 11:53:16 -0700

    Add licensing header to code files

--- scripts/job_apptrace_join.scala
+++ scripts/job_apptrace_join.scala
@@ -1,3 +1,8 @@
+// Copyright 2018 Lawrence Livermore National Security, LLC and other
+// ScrubJay Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 import scrubjay._
 
 val job_queue_meta = createCassandraMetaSource(sc, "cab_dat_2016", "job_queue_meta")
--- src/test/scala/testsuite/QueryParserSpec.scala
+++ src/test/scala/testsuite/QueryParserSpec.scala
@@ -1,3 +1,8 @@
+// Copyright 2018 Lawrence Livermore National Security, LLC and other
+// ScrubJay Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 package testsuite
 
 import scrubjay.query._
